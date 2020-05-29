757201075a882ad7d14605f155166df8df25d3cd
Avinash Singh <avs2805@gmail.com>
2020-05-29 14:33:07 -0700

    removed cmd_vel_cb_old and other unused parts of the code

--- sabertooth_ros_ctrl/scripts/cmd_vel_to_diff_wheels.py
+++ sabertooth_ros_ctrl/scripts/cmd_vel_to_diff_wheels.py
@@ -69,64 +69,6 @@ class cmd_vel_to_motors:
     def map_val(self, x, in_min, in_max, out_min, out_max):
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-    def cmd_vel_cb_old(self, vel):
-        # rospy.loginfo(rospy.get_caller_id() + "I heard %s", vel.linear.x)
-        # perform wheel math
-        # TODO: verify math
-        self.lin_speed = vel.linear.x
-        self.ang_speed = vel.angular.z
-        # rospy.loginfo(self.ang_speed)
-        # https://hackernoon.com/unicycle-to-differential-drive-courseras-control-of-mobile-robots-with-ros-and-rosbots-part-2-6d27d15f2010#1e59
-        self.w_l = ((2 * self.lin_speed) - (self.ang_speed * self.WHEEL_SEPARATION)) / (
-            2.0 * self.WHEEL_RADIUS
-        )
-        # self.w_r = (speed_lin / self.wheel_rad) - ((speed_ang * self.wheel_sep) / (2.0 * self.wheel_rad))
-        self.w_r = ((2 * self.lin_speed) + (self.ang_speed * self.WHEEL_SEPARATION)) / (
-            2.0 * self.WHEEL_RADIUS
-        )
-
-        rospy.loginfo(self.w_l)
-
-        # RIGHT MOTOR 1  - ( 1 - reverse - 64 - forward - 127)
-        # LEft MOTOR - ( 128 - reverse - 192 - forward - 255)
-        # self.rightmotorcmd = self.map_val (self.w_r, -15, 15, -127, 127)
-        # self.leftmotorcmd = self.map_val(self.w_l, -25, 25, -15, 15, -127, 127)
-        # self.leftmotorcmd = self.w_l
-        # self.rightmotorcmd = self.w_r
-
-        # self.leftmotorcmd = int(ceil(self.leftmotorcmd))
-        # if(self.leftmotorcmd < 128):
-        #     self.leftmotorcmd = 128
-        # elif(self.leftmotorcmd > 255):
-        #     self.leftmotorcmd = 255
-
-        # self.rightmotorcmd = int(ceil(self.rightmotorcmd))
-        # if(self.rightmotorcmd < 1):
-        #     self.rightmotorcmd = 1
-        # elif(self.rightmotorcmd > 127):
-        #     self.rightmotorcmd = 127
-        # while not rospy.is_shutdown():
-        self.cmd_msg.left = int(self.w_l)
-        self.cmd_msg.right = int(self.w_r)
-        self.pub_motor_cmd.publish(self.cmd_msg)
-        # self.pub_rmotor.publish(self.rightmotorcmd)
-
-        # rospy.loginfo(rospy.get_caller_id() + "left %f", self.rightmotorcmd)
-        # rospy.loginfo(rospy.get_caller_id() + "Right %f", self.leftmotorcmd)
-
-        # def talker(self):
-        #     pub = rospy.Publisher('chatter', String, queue_size=10)
-        #     rospy.init_node('talker', anonymous=True)
-        #     rate = rospy.Rate(10) # 10hz
-        #     while not rospy.is_shutdown():
-        #         hello_str = "hello world %s" % rospy.get_time()
-        #         rospy.loginfo(hello_str)
-        #         pub.publish(hello_str)
-        #         rate.sleep()
-        # def cmd_vel_sub(self):
-        #     rospy.init_node('cmd_vel_listener', anonymous=True)
-        #     rospy.Subscriber("cmd_vel", Twist, cmd_vel_cb, queue_size=1)
-
 
 if __name__ == "__main__":
     rospy.init_node("cmd_vel_to_motors_convertor")
